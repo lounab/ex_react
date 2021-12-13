db53a358af7d16b64c65ee574bc24b70a8753afb
Louna BORDET <louna.bordet@gmail.com>
2021-12-13 14:33:19 +0100

    toto

---
+++ component/Navbar.js
@@ -0,0 +1,14 @@
+import react, { useState, useEffect } from "react"
+import styles from '../styles/Navbar.module.css'
+
+export default function Navbar() {
+
+    return (
+        <div className={styles.navcontainer}>
+            <p>toto</p>
+            <p>tata</p>
+            <p>titi</p>
+            
+        </div>
+    )
+}
